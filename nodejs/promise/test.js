cf0cd0475c6384336bfdf8cab003e2996d91fa65
gaoxiaobo <gaoxiaobobo@qq.com>
2016-03-17 09:23:29 +0100

    测试less

--- nodejs/promise/test.js
+++ nodejs/promise/test.js
@@ -1,10 +1,1 @@
-function* gen(x){
-  var y = yield x + 2;
-  return y;
-}
-var g = gen(1);
-console.log(g);
- 
-console.log(g.next());// { value: 3, done: false }
-
-console.log(g.next());// { value: undefined, done: true }
+console.log(233);
